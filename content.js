365d9b76f3ce592c8638e237d45bb842e66d735f
Gabriel Vanjura <gabrielvanjura@outlook.com>
2020-07-25 14:25:45 -0300

    remove console.log

--- content.js
+++ content.js
@@ -1,26 +1,18 @@
-console.log("Extensão");
-
 const interval = setInterval(() => {
     const header = document.querySelector("._1QUKR");
     if(header){
-        console.log(header);
+        console.log("Extensão em funcionamento");
         clearInterval(interval);
-
         const button = document.createElement("button");
         button.innerHTML = "2x";
         button.classList.add("speedAudioButtonStyle");
-
         button.addEventListener("click", () => {
             const audios = document.querySelectorAll("audio");
-            console.log(audios);
             audios.forEach(audio => {
                 console.log(audio);
                 audio.playbackRate = 2;
             });
         })
-
         header.appendChild(button);
-    }else{
-        console.log("não achou");
     }
 }, 1000);
